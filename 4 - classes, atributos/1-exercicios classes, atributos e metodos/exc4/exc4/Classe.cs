a249dcb15c627593b52ee32b034a98d9315799b6
philipheferreira <philipheferreira@gmail.com>
2025-11-05 13:54:35 -0300

    Feat: things i forget

--- 4 - classes, atributos/1-exercicios classes, atributos e metodos/exc4/exc4/Classe.cs
+++ 4 - classes, atributos/1-exercicios classes, atributos e metodos/exc4/exc4/Classe.cs
@@ -4,8 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/* Criação de uma unica classe que salva uma unica variavel que vai ser reutilizada em outra chamada */
+
 namespace Treino{
     internal class Classe{
-        public int valor = 20;
+        public int valor = 20; /* variavel declarada dentro da classe que recebe apenas um unico valor numerico */
     }
 }
