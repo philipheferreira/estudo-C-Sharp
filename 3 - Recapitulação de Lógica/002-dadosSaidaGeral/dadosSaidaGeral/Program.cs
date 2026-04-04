22b9ac83c9dfdb9d7c58e71865a9fd0af4bb5b7e
philipheferreira <philipheferreira@gmail.com>
2026-04-04 08:11:24 -0300

    Feat: new cap for operations

---
+++ 3 - Recapitulação de Lógica/002-dadosSaidaGeral/dadosSaidaGeral/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program{
+        static void Main(string[] args) { 
+        bool completo = false;
+        char genero = 'F';
+        char letra = '';
+        byte n1 = 126;
+        int n2 = 1000;
+        int n3 = 2147483647;
+        long n4 = 2147483648L;
+
+        Console.WriteLine("O valor booleano é: " + completo);
+        Console.WriteLine("O valor do gênero é: " + genero);
+        Console.WriteLine("O valor da letra é: " + letra);
+        Console.WriteLine("O valor do byte é: " + n1);
+        Console.WriteLine("O valor do int em n2 é: " + n2);
+        Console.WriteLine("O valor do int em n3 é: " + n3);
+        Console.WriteLine("O valor do long é: " + n4);
+    }
+}
