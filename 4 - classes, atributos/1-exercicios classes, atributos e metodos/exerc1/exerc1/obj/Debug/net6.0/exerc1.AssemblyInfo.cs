141ddb45fa2fb6e74b9ed87e20e4b666c5beb8e1
philipheferreira <philipheferreira@gmail.com>
2026-04-04 07:59:04 -0300

    Feat: things i forget

--- 4 - classes, atributos/1-exercicios classes, atributos e metodos/exerc1/exerc1/obj/Debug/net6.0/exerc1.AssemblyInfo.cs
+++ 4 - classes, atributos/1-exercicios classes, atributos e metodos/exerc1/exerc1/obj/Debug/net6.0/exerc1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exerc1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+986dc61de04b81c74c34e3528fef7b0903a0ed67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23375c79e28cd99e238778a722dd4bf2a6f456f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("exerc1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exerc1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
